421808355c6534d9f1ab353a7753c2a16590695e
kimhyocheol <khs970629@naver.com>
2024-02-25 19:45:45 +0900

    완료

---
+++ main.js
@@ -0,0 +1,10 @@
+new Swiper('.intro-sub7 .swiper-container', {
+    // autoplay : true,
+    // loop: true,
+
+    slidesPerView: 3,
+    navigation: {
+        prevEl:'.intro-sub7 .swiper-prev',
+        nextEl:'.intro-sub7 .swiper-next'
+    }
+});
